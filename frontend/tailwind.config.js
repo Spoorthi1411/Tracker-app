1f25baad8fbb61b69cff41d6c2cfbe6f2d6bae3a
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-07-12 23:14:59 +0530

    signup done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/daisyui/dist/**/*.js", // <- Add this line
+    "./src/**/*.{js,ts,jsx,tsx}",  // adjust if you're using different folders or file types
   ],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
 }
-
 
