f33fb9adb927bae6b3748f4df4afb2d808e158de
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-07-13 22:26:59 +0530

    uidesign done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
@@ -8,13 +6,9 @@ import SignUp from './pages/signup/SignUp';
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          {/* <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} /> */}
-        </Routes>
-      </Router>
+      <Home />
+      {/* <Login/>
+      <SignUp/> */}
     </div>
   );
 }
