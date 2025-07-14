4b68fbc4edb4d7bdbb7734284186b8caf9983f64
sundar-1116 <kondala.20234084@mnnit.ac.in>
2025-07-14 08:16:30 +0530

    done with get users

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectTOMongoDb from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -15,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 //app.get("/",(req,res) => {
 //    res.send("Hello World !!");
