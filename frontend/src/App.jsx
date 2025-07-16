51731418747ce80164b2f9ee0b72d299afad0d21
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-07-16 18:09:14 +0530

    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,9 @@ import SignUp from './pages/signup/SignUp';
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home />
-      {/* <Login/>
-      <SignUp/> */}
+      {/* <Home /> */}
+      {/* <Login/> */}
+      <SignUp/>
     </div>
   );
 }
--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GenderCheckbox = () => {
+  const [selectedGender, setSelectedGender] = useState('');
+
+  const handleCheckboxChange = (gender) => {
+    setSelectedGender((prev) => (prev === gender ? '' : gender));
+  };
+
   return (
     <div className='mt-2 flex gap-x-6 items-center'>
       <div className='form-control'>
         <label className='label gap-2 cursor-pointer flex items-center'>
-          <input type='checkbox' className='checkbox border-slate-900' />
+          <input
+            type='checkbox'
+            className='checkbox border-slate-900'
+            checked={selectedGender === 'male'}
+            onChange={() => handleCheckboxChange('male')}
+          />
           <span className='label-text'>Male</span>
         </label>
       </div>
       <div className='form-control'>
         <label className='label gap-2 cursor-pointer flex items-center'>
-          <input type='checkbox' className='checkbox border-slate-900' />
+          <input
+            type='checkbox'
+            className='checkbox border-slate-900'
+            checked={selectedGender === 'female'}
+            onChange={() => handleCheckboxChange('female')}
+          />
           <span className='label-text'>Female</span>
         </label>
       </div>
