37ac1989b5078768bd9eb9404c0082e23d8b2176
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-07-19 13:17:00 +0530

    AuthContext Completed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",  // adjust if you're using different folders or file types
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
-
+};
