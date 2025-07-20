a6a0a2c9a277b3b5d66c39feac4953f850aaffd5
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-07-20 17:34:30 +0530

    Fixed Errors

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,31 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/Authcontext";
 
 const useLogout = () => {
-  const [loading,setLoading] = useState(false)
-const{setAuthUser} = userAuthContext()
-  const logout =async () =>{
-    setLoading(true)
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const logout = async () => {
+    setLoading(true);
     try {
-        const res = await fetch("https://myapp-backend.onrender.com/api/auth/signup", {
+      const res = await fetch("https://myapp-backend.onrender.com/api/auth/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" }
+      });
 
-            method:"POST",
-            headers:{"Content-Type":"application/json"}
-        });
-        const data = await res.json()
-        if(data.error){
-            throw new Error(data.error)
-        }
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
 
-        localStorage.removeItem("chat-user")
-        setAuthUser(null)
+      localStorage.removeItem("chat-user");
+      setAuthUser(null);
     } catch (error) {
-        Toast.error(error.message)
-    }finally{
-        setLoading(false)
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
-  return { loading, logout};
+  return { loading, logout };
 }
 
-export default useLogout
+export default useLogout;
